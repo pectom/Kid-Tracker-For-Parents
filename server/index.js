26e7875e662d802e7923f25aa4cfc239529faef2
Tomasz Pecak <pecatoma@gmail.com>
2018-11-27 19:38:32 +0100

    Proxy Setup

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ app.get('/',(req,res) => {
     res.send('Hello World!');
 });
 
-
-app.listen(5000, () => {
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
     console.log('Example app listening on port 5000!');
 });
