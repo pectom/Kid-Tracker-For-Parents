eac9d4ff23984c76d9a7e72a4b08da62e2ea6128
Przemysław Peterman <przemyslaw.peterman@uj.edu.pl>
2018-12-16 18:29:34 +0100

    add wrong credentials test

--- server/server_spec/loginPage.js
+++ server/server_spec/loginPage.js
@@ -1,6 +1,7 @@
 const selenium = require('selenium-webdriver');
 
-describe('Selenium Tutorial', function() {
+describe('Login page', function() {
+
     jasmine.getEnv().defaultTimeoutInterval = 200000;
     let chromeOptions = {'args': ['--no-sandbox']};
     let chromeCapabilities = selenium.Capabilities.chrome().set('chromeOptions', chromeOptions);
@@ -26,7 +27,31 @@ describe('Selenium Tutorial', function() {
             });
     });
 
-    it('Should be logged', function (done) {
+    it('Login with wrong credentials', function (done) {
+        driver.wait(until.elementLocated(By.id("login-email")), 10000).then(
+            () => {
+                driver.findElement(By.id('login-email')).sendKeys('wrong@credentials.com').then(
+                    () => {
+                        driver.findElement(By.id('login-password')).sendKeys('wrongCredentials').then(
+                            () => {
+                                driver.findElement(By.id('login-button')).click().then(
+                                    () => {
+                                        setTimeout(
+                                            () => {
+                                                driver.getCurrentUrl().then(function(value) {
+                                                    expect(value).toContain('/');
+                                                    done();
+                                                })}, 3000);
+                                    }
+                                )
+                            })
+                    }
+                )
+            }
+        );
+    });
+
+    it('Login with good credentials', function (done) {
         driver.wait(until.elementLocated(By.id("login-email")), 10000).then(
             () => {
                 driver.findElement(By.id('login-email')).sendKeys('aga@aga.com').then(
@@ -38,7 +63,6 @@ describe('Selenium Tutorial', function() {
                                         setTimeout(
                                             () => {
                                                 driver.getCurrentUrl().then(function(value) {
-                                                    console.log(value);
                                                     expect(value).toContain('/dashboard');
                                                     done();
                                             })}, 3000);
