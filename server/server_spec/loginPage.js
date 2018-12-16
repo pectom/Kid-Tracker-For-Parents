99d4192b6e40dd6be0170c762753504cabbd5540
Przemysław Peterman <przemyslaw.peterman@uj.edu.pl>
2018-12-16 18:34:49 +0100

    then -> await

--- server/server_spec/loginPage.js
+++ server/server_spec/loginPage.js
@@ -18,60 +18,39 @@ describe('Login page', function() {
             .then(done)
     });
 
-    it('Should be on the home page', function (done) {
-        driver.wait(until.elementLocated(By.id("login-email")), 5000);
-        driver.findElement(By.id('login-email'))
+    it('Should be on the home page', async function (done) {
+        await driver.wait(until.elementLocated(By.id("login-email")), 5000);
+        await driver.findElement(By.id('login-email'))
             .then(el => {
                 expect(el).toBeDefined();
                 done();
             });
     });
 
-    it('Login with wrong credentials', function (done) {
-        driver.wait(until.elementLocated(By.id("login-email")), 10000).then(
+    it('Login with wrong credentials', async function (done) {
+        await driver.wait(until.elementLocated(By.id("login-email")), 10000);
+        await driver.findElement(By.id('login-email')).sendKeys('wrong@credentials.com');
+        await driver.findElement(By.id('login-password')).sendKeys('wrongCredentials');
+        await driver.findElement(By.id('login-button')).click();
+        setTimeout(
             () => {
-                driver.findElement(By.id('login-email')).sendKeys('wrong@credentials.com').then(
-                    () => {
-                        driver.findElement(By.id('login-password')).sendKeys('wrongCredentials').then(
-                            () => {
-                                driver.findElement(By.id('login-button')).click().then(
-                                    () => {
-                                        setTimeout(
-                                            () => {
-                                                driver.getCurrentUrl().then(function(value) {
-                                                    expect(value).toContain('/');
-                                                    done();
-                                                })}, 3000);
-                                    }
-                                )
-                            })
-                    }
-                )
-            }
-        );
+                driver.getCurrentUrl().then(function(value) {
+                    expect(value).toContain('/');
+                    done();
+                })}, 3000);
     });
 
-    it('Login with good credentials', function (done) {
-        driver.wait(until.elementLocated(By.id("login-email")), 10000).then(
+    it('Login with good credentials', async function (done) {
+        await driver.wait(until.elementLocated(By.id("login-email")), 10000);
+        await driver.findElement(By.id('login-email')).sendKeys('aga@aga.com');
+        await driver.findElement(By.id('login-password')).sendKeys('aaa');
+        await driver.findElement(By.id('login-button')).click();
+        setTimeout(
             () => {
-                driver.findElement(By.id('login-email')).sendKeys('aga@aga.com').then(
-                    () => {
-                        driver.findElement(By.id('login-password')).sendKeys('aaa').then(
-                            () => {
-                                driver.findElement(By.id('login-button')).click().then(
-                                    () => {
-                                        setTimeout(
-                                            () => {
-                                                driver.getCurrentUrl().then(function(value) {
-                                                    expect(value).toContain('/dashboard');
-                                                    done();
-                                            })}, 3000);
-                                    }
-                                )
-                            })
-                    }
-                )
-            }
-        );
-    });
+                driver.getCurrentUrl().then(function(value) {
+                    expect(value).toContain('/dashboard');
+                    done();
+            })}, 3000);
+    })
+
 });
