7fe3afbf67515810a44289a48783d10c720e11bd
AgaZadworny <agnieszka.zadworny@gmail.com>
2018-12-16 21:33:58 +0100

    register tests

---
+++ server/server_spec/registerPage.js
@@ -0,0 +1,75 @@
+const selenium = require('selenium-webdriver');
+
+describe('Login page', function() {
+
+    jasmine.getEnv().defaultTimeoutInterval = 200000;
+    let chromeOptions = {'args': ['--no-sandbox']};
+    let chromeCapabilities = selenium.Capabilities.chrome().set('chromeOptions', chromeOptions);
+    var driver = undefined;
+    const By = selenium.By;
+    let until = selenium.until;
+    beforeEach(function(done) {
+        driver = new selenium.Builder().withCapabilities(chromeCapabilities).build();
+        driver.get('http://localhost:3000/').then(done);
+    });
+
+    afterEach(function(done) {
+        driver.quit()
+            .then(done)
+    });
+
+    it('Should be on register tab', async function (done) {
+        await driver.wait(until.elementLocated(By.id("register-tab")), 5000);
+        await driver.findElement(By.id('register-tab')).click();
+        await driver.findElement(By.id('register-email'))
+            .then(el => {
+                expect(el).toBeDefined();
+                done();
+            });
+    });
+
+    it('Register with wrong credentials', async function (done) {
+        await driver.wait(until.elementLocated(By.id("register-tab")), 10000);
+        await driver.findElement(By.id('register-tab')).click();
+        await driver.findElement(By.id('register-name')).sendKeys('wrong');
+        await driver.findElement(By.id('register-surname')).sendKeys('credentials');
+        await driver.findElement(By.id('register-email')).sendKeys('wrong.credentials.com');
+        await driver.findElement(By.id('register-password')).sendKeys('wrongCredentials');
+        await driver.findElement(By.id('register-password2')).sendKeys('wrongCredentials');
+        await driver.findElement(By.id('register-button')).click();
+
+        await driver.findElement(By.id('login-tab')).click();
+        await driver.findElement(By.id('login-email')).sendKeys('wrong.credentials.com');
+        await driver.findElement(By.id('login-password')).sendKeys('wrongCredentials');
+        await driver.findElement(By.id('login-button')).click();
+        setTimeout(
+            () => {
+                driver.getCurrentUrl().then(function(value) {
+                    expect(value).toContain('/');
+                    done();
+                })}, 3000);
+    });
+
+    xit('Register with good credentials', async function (done) {
+        await driver.wait(until.elementLocated(By.id("register-tab")), 10000);
+        await driver.findElement(By.id('register-tab')).click();
+        await driver.findElement(By.id('register-name')).sendKeys('aga');
+        await driver.findElement(By.id('register-surname')).sendKeys('aga');
+        await driver.findElement(By.id('register-email')).sendKeys('aga@aga.pl');
+        await driver.findElement(By.id('register-password')).sendKeys('aga');
+        await driver.findElement(By.id('register-password2')).sendKeys('aga');
+        await driver.findElement(By.id('register-button')).click();
+
+        await driver.findElement(By.id('login-tab')).click();
+        await driver.findElement(By.id('login-email')).sendKeys('aga@aga.pl');
+        await driver.findElement(By.id('login-password')).sendKeys('aga');
+        await driver.findElement(By.id('login-button')).click();
+        setTimeout(
+            () => {
+                driver.getCurrentUrl().then(function(value) {
+                    expect(value).toContain('/dashboard');
+                    done();
+                })}, 3000);
+    })
+
+});
