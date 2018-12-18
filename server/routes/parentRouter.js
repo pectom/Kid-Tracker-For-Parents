d9054765d48cc4f7c556df59d84fcc9190909b8c
Tomasz Pecak <pecatoma@gmail.com>
2018-12-18 13:51:11 +0100

    added deleting parent

--- server/routes/parentRouter.js
+++ server/routes/parentRouter.js
@@ -1,9 +1,11 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
 const childrenRouter  = require('./parentRoutes/childrenRoutes');
 const areaRouter = require('./parentRoutes/areaRoutes');
 const ruleRouter = require('./parentRoutes/ruleRoutes');
 const locationRouter = require('./parentRoutes/locationRoutes');
+
+const User = mongoose.model('users');
 
 const parentRouter = express.Router();
 
@@ -11,5 +13,15 @@ parentRouter.use('/children',childrenRouter);
 parentRouter.use('/areas',areaRouter);
 parentRouter.use('/rules', ruleRouter);
 parentRouter.use('/location', locationRouter);
+
+parentRouter.delete('/',async (req,res,next) =>{
+   try{
+       await User.deleteOne({_id: req.user._id});
+       res.send({delete: "ok"}).status(204);
+   } catch (e) {
+       console.log(e);
+       res.status(400).send(e);
+   }
+});
 
 module.exports = parentRouter;
