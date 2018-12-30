b455ae6863d2d00ee3f1e0253c55f1da31ab530c
Tomasz Pecak <pecatoma@gmail.com>
2018-12-30 21:17:45 +0100

    Add notification token

--- server/config/prod.js
+++ server/config/prod.js
@@ -3,7 +3,11 @@ module.exports = {
     googleClientID: process.env.GOOGLE_CLIENT_ID,
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
     mongoURI: process.env.MONGO_URI,
-    cookieKey: process.env.COOKIE_KEY
+    cookieKey: process.env.COOKIE_KEY,
+    firebaseProjectId: process.env.FIREBASE_PROJECT_ID,
+    firebaseClientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    firebasePrivateKey: process.env.FIREBASE_PRIVATE_KEY,
+    fireBaseDB: process.env.FIREBASE_DB
 };
 
 
--- server/routes/parentRoutes/registrationRoutes.js
+++ server/routes/parentRoutes/registrationRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const registartionRouter = express.Router();
 const {generatePassword} = require('../../utils/passwordManager');
+const requireParent = require('../../middlewares/requireParent');
 
 const User = mongoose.model('users');
 
@@ -28,5 +29,24 @@ registartionRouter.post('/',async(req,res) =>{
     }
 
 });
-
+registartionRouter.post('/token',requireParent,async (req,res) =>{
+    const {firebaseToken} =req.body;
+    if(firebaseToken){
+        try {
+            User.updateOne({
+                _id: req.user._id
+            },{
+                firebaseToken
+            });
+            res.status(400).send({
+                success: "ok"
+            });
+        }catch (e) {
+            console.log(e);
+            res.status(400).send(e)
+        }
+    }else{
+        res.status(404).send("Incomplete request")
+    }
+});
 module.exports = registartionRouter;
